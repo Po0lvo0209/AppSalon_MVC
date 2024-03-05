d65c388176ddb7a9ae978e605e3d3f594301e8a9
Diego Bermudez <diegobl9do@gmail.com>
2024-03-04 20:08:32 -0500

    first comit

---
+++ views/auth/crear-cuenta.php
@@ -0,0 +1,43 @@
+<h1 class="nombre-pagina">Crear Cuenta</h1>
+<p class="descripcion-pagina">Llena el siguiente formulario para crear una cuenta</p>
+
+
+<?php
+include_once __DIR__ . "/../templates/alertas.php"
+?>
+
+<form action="/crear-cuenta" method="POST" class="formulario">
+    <div class="campo">
+        <label for="nombre">Nombre</label>
+        <input type="text" id="nombre" placeholder="Tu Nombre" name="nombre" value="<?php echo s($usuario->nombre) ?>">
+    </div>
+
+    <div class="campo">
+        <label for="apellido">Apellido</label>
+        <input type="text" id="apellido" placeholder="Tu apellido" name="apellido" value="<?php echo s($usuario->apellido) ?>">
+    </div>
+
+    <div class="campo">
+        <label for="telefono">Télefono</label>
+        <input type="tel" id="apellido" placeholder="Tu telefono" maxlength="9" name="telefono" value="<?php echo s($usuario->telefono) ?>">
+    </div>
+
+    <div class="campo">
+        <label for="email">E-mail</label>
+        <input type="email" id="apellido" placeholder="Tu email" name="email" value="<?php echo s($usuario->email) ?>">
+    </div>
+
+    <div class="campo">
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="Tu password" name="password">
+    </div>
+
+    <input type="submit" value="Crear Cuenta" class="boton">
+
+</form>
+
+
+<div class="acciones">
+    <a href="/">¿Ya tienes una cuenta? Inicia Sesión</a>
+    <a href="/olvide">¿Olvidaste tu password?</a>
+</div>
